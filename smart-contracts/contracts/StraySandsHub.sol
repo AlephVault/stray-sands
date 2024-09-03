0b5b994a9d90b5796c3d9f9fac7328bdbc368ef8
Luis Masuelli <luisfmasuelli@gmail.com>
2024-09-03 13:34:03 -0500

    Adding the concept of relay data.

--- smart-contracts/contracts/StraySandsHub.sol
+++ smart-contracts/contracts/StraySandsHub.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
 /**
@@ -14,12 +15,71 @@ import "@openzeppelin/contracts/utils/Base64.sol";
  * Your contract should define a way to mint tokens, and
  * optionally a way to burn tokens.
  */
-contract StraySandsHub is ERC721 {
+contract StraySandsHub is ERC721, Ownable {
+    /**
+     * Each relay will have its own data.
+     */
+    struct RelayData {
+        /**
+         * This flag is true for any existing relay record.
+         */
+        bool exists;
+
+        /**
+         * The name of the relay. This is a very visual one.
+         * Examples are: "Tidal Flare" or "Academia Blockchain".
+         */
+        string name;
+
+        /**
+         * An arbitrary description for the hub. Examples are:
+         * "Main DragonShark games repository" or "The storage
+         * for the main Academia Blockchain contents".
+         */
+        string description;
+
+        /**
+         * An image for the relay. The image must be a URL, and
+         * can be "" or "about:blank", or any valid URL, being
+         * it "http:", "ipfs:" or "data:".
+         */
+        string image;
+
+        /**
+         * The URL of the owned resource's redemption.
+         */
+        string redemptionUrl;
+
+        /**
+         * The address that will be assigned to the relay. This
+         * one must be obtained. Any operation the relay wants
+         * to do in-chain will be done with THIS address, and
+         * not with the address of the token's owner.
+         */
+        address relayAddress;
+
+        /**
+         * The tags for this relay. This can be changed later
+         * and it is NOT binding but hints what kind of contents
+         * will it serve. This can be changed later and will not
+         * attempt to bind which service can be provided, which
+         * strongly implies the OLD contents here as well, but
+         * just a hint for the users for the current state of the
+         * relay's contents or trend.
+         */
+        bytes32[] tags;
+    }
+
+    /**
+     * The registered relays.
+     */
+    mapping(uint256 => RelayData) public relays;
+
     /**
      * Feel free to setup your ERC721 name and symbol as you please
      * if you change your mind.
      */
-    constructor() ERC721("Stray Sands Relays", "SSANDS") {}
+    constructor() ERC721("Stray Sands Relays", "SSANDS") Ownable(msg.sender) {}
 
     /**
      * Implement your own logic to mint tokens by invoking _safeMint
@@ -40,11 +100,11 @@ contract StraySandsHub is ERC721 {
     function _getTokenMetadata(uint256 tokenId) internal view returns (
         string memory name, string memory description, string memory image
     ) {
-        _requireOwned(tokenId);
-        // TODO implement the actual retrieval here.
-        name = "";
-        description = "";
-        image = "";
+        require(relays[tokenId].exists, "StraySands: Invalid token");
+        RelayData storage data = relays[tokenId];
+        name = data.name;
+        description = data.description;
+        image = data.image;
     }
 
     /**
